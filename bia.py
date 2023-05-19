0be97440a976ee0114131c5f354b4e1474717223
biabronzere <127956367+biabronzere@users.noreply.github.com>
2023-05-19 11:50:42 -0300

    Add files via upload

---
+++ bia.py
@@ -0,0 +1,33 @@
+print("ola bia", end=".")
+
+print("bia linda")
+
+print("isa veia")
+
+print("maria chata")
+
+print("joao insuportavel")
+
+print("mensagem \"sa")
+
+print("joao insuportavel")
+
+print(1+1)
+
+print("10² =,10**3")
+
+print("10³", pow(10,3))
+
+nome = str ("A")
+x = int(1)
+x = 1+1
+y = True
+
+D1 = 5.51
+D2 = "5"
+print(int(D1 + int(D2)))
+print(round(D1 + int(D2)))
+
+
+
+
